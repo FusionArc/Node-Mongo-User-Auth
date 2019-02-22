70977275f933ec9eb04e4624f9aec9c64dd58780
David Barrow <dave@fusion-arc.uk>
2019-02-22 23:16:12 +0000

    Updated

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://davecheez:batfink21@nodeauthsandbox-shard-00-00-wzudu.gcp.mongodb.net:27017,nodeauthsandbox-shard-00-01-wzudu.gcp.mongodb.net:27017,nodeauthsandbox-shard-00-02-wzudu.gcp.mongodb.net:27017/test?ssl=true&replicaSet=NodeAuthSandbox-shard-0&authSource=admin&retryWrites=true',
+  secret: 'yoursecret'
+}
